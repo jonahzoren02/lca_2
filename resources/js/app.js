e1db431a719e5681e7feb965c6ace421e68bfd69
Jonah Garriazo <jonahzoren01@gmail.com>
2026-03-19 18:06:30 -0500

    primer inicio

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import "./bootstrap";
+
+var swiper = new Swiper(".default-carousel", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-left",
+    },
+});
